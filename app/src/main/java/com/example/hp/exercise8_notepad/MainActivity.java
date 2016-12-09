de6e1becca4e456849ec1a2cb4cd36dbbcf690f4
hp-PC\hp <Mahdi.heravi@gmail.com>
2016-12-09 17:10:36 +0330

    Design Dialog

--- app/src/main/java/com/example/hp/exercise8_notepad/MainActivity.java
+++ app/src/main/java/com/example/hp/exercise8_notepad/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.hp.exercise8_notepad;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btndialog = (Button) findViewById(R.id.btndialog);
+        btndialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
     }
 }
